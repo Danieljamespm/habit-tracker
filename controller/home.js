aeb0a8ac4de4bc06839e54e8cf370027dd64c5c3
Danny Mettler <danieljamespm@gmail.com>
2024-10-18 22:36:12 -0400

    had an error when trying to add a habit because I had REQUEST instead of REQ in the habits.create

--- controller/home.js
+++ controller/home.js
@@ -1,5 +1,44 @@
+//habits model for storing habit data
+const Habits = require('../models/habits')
+
+
 module.exports = {
-    home: (req,res)=>{
-        res.render('home.ejs')
+    
+    home: async (req,res)=>{
+        try {
+            //getting all the habits from database
+            //rendering homepage
+            const myHabits = await Habits.find({})
+            res.render('home.ejs')
+            //giving all the habits a variable name
+            myHabits: myHabits
+            
+        } catch (err) {
+            console.log(err)
+        }
+
+    },
+    createHabit: async (req,res)=>{
+        try {
+            //get today's date
+            let date = new Date().toString()
+            date = `${date.slice(4,15)}`
+            //weekly dates
+            const weekStatus = Array(7).fill(null)
+
+            //create new habit in MongoDB
+            const doc = await Habits.create({
+                name:req.body.newHabit,
+                createdAt:date,
+                weeklyStatus:weekStatus,
+                completedDays:0
+            })
+            console.log(`Habit ${req.body.newHabit} has been added!`)
+            //return response
+            res.redirect('/')
+
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
