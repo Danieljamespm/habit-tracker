4dfa4cb91562a483f58880052ea090cf118c6ab0
Danny Mettler <danieljamespm@gmail.com>
2024-10-18 23:42:13 -0400

    added create habit post route.

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const homeController = require('../controller/home')
 
+//route for homepage
 router.get('/', homeController.home)
+
+//route for post request to create habits
 router.post('/createHabit', homeController.createHabit)
 
 
